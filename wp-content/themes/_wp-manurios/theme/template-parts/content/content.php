44b239e5a963f5efdd75a2d99470e215dc9b04a6
Dayvson Marques <dayvson.marques@gmail.com>
2025-11-10 13:00:44 -0300

    _tw: theme based - wordpress tailwindcss theme starter

---
+++ wp-content/themes/_wp-manurios/theme/template-parts/content/content.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package _wp-manurios
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header">
+		<?php
+		if ( is_sticky() && is_home() && ! is_paged() ) {
+			printf( '<span">%s</span>', esc_html_x( 'Featured', 'post', '_wp-manurios' ) );
+		}
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		endif;
+		?>
+	</header><!-- .entry-header -->
+
+	<?php _wp_manurios_post_thumbnail(); ?>
+
+	<div <?php _wp_manurios_content_class( 'entry-content' ); ?>>
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before' => '<div>' . __( 'Pages:', '_wp-manurios' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php _wp_manurios_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-${ID} -->
